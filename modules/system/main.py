1b53df20ef2050737446571bba1128883eb32ffe
papajoke <papajoke@manjaro.fr>
2025-06-23 06:29:12 +0200

    plugin : another way for standalone

---
+++ modules/system/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import json
+import subprocess
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QTextEdit, QWidget
+
+
+class SystemWidget(QWidget):
+    def __init__(self, parent: QWidget | None):
+        super().__init__(parent=parent)
+        self.setWindowTitle("System Info")
+        self.mlayout = QHBoxLayout()
+        self.setLayout(self.mlayout)
+
+        self._set()
+
+    def _set(self) -> None:
+        """inxi infos"""
+        try:
+            result = subprocess.run(
+                "/usr/bin/inxi -Fx --output json --output-file print",
+                capture_output=True,
+                text=True,
+                shell=True,
+                check=True,
+            )
+            data = result.stdout
+        except subprocess.CalledProcessError as e:
+            data = f"Error running inxi: {e}"
+
+        # print(data)
+        data_json = json.loads(data)
+        txt = ""
+        for rub in data_json:
+            for key, value in rub.items():
+                txt = f"{txt}<br><br> <b>{key.split('#')[-1].upper()}</b><br>"
+                for srub in value:
+                    for key, val in dict(sorted(srub.items())).items():
+                        if val in ("N/A", "<superuser required>"):
+                            continue
+                        if val:
+                            txt = f"{txt}         {key.split('#')[-1]} : {val}<br>"
+                        else:
+                            txt = f"{txt}     <b>{key.split('#')[-1]}</b> :<br>"
+
+        # label = QLabel(txt, parent=self, margin=20)
+        edit = QTextEdit(readOnly=True)
+        edit.setHtml(txt.replace(" ", "&nbsp;"))
+        # edit.setCurrentFont(QFontDatabase.systemFont(QFontDatabase.SystemFont.FixedFont))
+        self.mlayout.addWidget(edit)
+
+
+def main():
+    app = QApplication([])
+    win = SystemWidget(None)
+    win.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
--- modules/system/plugin.py
+++ modules/system/plugin.py
@@ -1,58 +1,11 @@
-import json
-import subprocess
 import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent))
 from .._plugin.base import PluginBase
+from main import SystemWidget
 
 sys.path.pop(0)
-from PySide6.QtWidgets import QHBoxLayout, QTextEdit, QWidget
-
-
-class SystemWidget(QWidget):
-    def __init__(self, parent: QWidget | None):
-        super().__init__(parent=parent)
-        self.setWindowTitle(Plugin.get_title())
-        self.mlayout = QHBoxLayout()
-        self.setLayout(self.mlayout)
-
-        self._set()
-
-    def _set(self) -> None:
-        """inxi infos"""
-        try:
-            result = subprocess.run(
-                "/usr/bin/inxi -Fx --output json --output-file print",
-                capture_output=True,
-                text=True,
-                shell=True,
-                check=True,
-            )
-            data = result.stdout
-        except subprocess.CalledProcessError as e:
-            data = f"Error running inxi: {e}"
-
-        # print(data)
-        data_json = json.loads(data)
-        txt = ""
-        for rub in data_json:
-            for key, value in rub.items():
-                txt = f"{txt}<br><br> <b>{key.split('#')[-1].upper()}</b><br>"
-                for srub in value:
-                    for key, val in dict(sorted(srub.items())).items():
-                        if val in ("N/A", "<superuser required>"):
-                            continue
-                        if val:
-                            txt = f"{txt}         {key.split('#')[-1]} : {val}<br>"
-                        else:
-                            txt = f"{txt}     <b>{key.split('#')[-1]}</b> :<br>"
-
-        # label = QLabel(txt, parent=self, margin=20)
-        edit = QTextEdit(readOnly=True)
-        edit.setHtml(txt.replace(" ", "&nbsp;"))
-        # edit.setCurrentFont(QFontDatabase.systemFont(QFontDatabase.SystemFont.FixedFont))
-        self.mlayout.addWidget(edit)
 
 
 class Plugin(PluginBase):
