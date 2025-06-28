9958faebd950c2a386a8578aba23c5e7c8e4e742
papajoke <papajoke@manjaro.fr>
2025-06-28 21:11:57 +0200

    hello : color icons

--- modules/hello/ui/hello.py
+++ modules/hello/ui/hello.py
@@ -1,12 +1,16 @@
+import re
 from pathlib import Path
 from textwrap import dedent
 
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QIcon, QPixmap
-from PySide6.QtWidgets import QGridLayout, QLabel, QMenu, QToolButton, QWidget
+from PySide6.QtGui import QIcon, QPainter, QPalette, QPixmap
+from PySide6.QtSvg import QSvgRenderer
+from PySide6.QtWidgets import QApplication, QGridLayout, QLabel, QMenu, QToolButton, QWidget
 
 
 class HelloMain(QWidget):
+    SIZE_ICO = 44
+
     def __init__(self, parent: QWidget | None):
         super().__init__(parent=parent)
         self.setWindowTitle("Manjaro Hello")
@@ -15,7 +19,7 @@ class HelloMain(QWidget):
         self.setLayout(layout)
 
         icon = QIcon(str(Path(__file__).parent.parent / "assets/manjaro.svg")).pixmap(72, 72)
-        label = QLabel(margin=10, alignment=Qt.Alignment.AlignCenter)
+        label = QLabel(margin=10, alignment=Qt.AlignmentFlag.AlignCenter)
         label.setPixmap(icon)
         layout.addWidget(label, 0, 0, 1, 3)
 
@@ -32,15 +36,15 @@ class HelloMain(QWidget):
             textFormat=Qt.TextFormat.AutoText,
             wordWrap=True,
             margin=5,
-            alignment=Qt.Alignment.AlignLeft,
+            alignment=Qt.AlignmentFlag.AlignLeft,
         )
         layout.addWidget(label, 1, 0, 1, 3)
 
         y = 2
         btn = QToolButton(
             parent=self,
-            icon=self.get_icon("text-x-readme", 32),
-            iconSize=QSize(32, 32),
+            icon=self.get_icon("text-x-readme", self.SIZE_ICO),
+            iconSize=QSize(self.SIZE_ICO, self.SIZE_ICO),
             text="Documentation",
             popupMode=QToolButton.ToolButtonPopupMode.InstantPopup,
         )
@@ -52,14 +56,14 @@ class HelloMain(QWidget):
         menu.addAction(self.get_icon("gnome-cd"), "release info")
         menu.addAction(self.get_icon("wikipedia"), "Wiki")
         btn.setMenu(menu)
-        layout.addWidget(btn, y, 0, alignment=Qt.Alignment.AlignCenter)
+        layout.addWidget(btn, y, 0, alignment=Qt.AlignmentFlag.AlignCenter)
 
         btn = QToolButton(
             parent=self,
             text="Support",
             popupMode=QToolButton.ToolButtonPopupMode.InstantPopup,
-            icon=self.get_icon("forum", 32),
-            iconSize=QSize(32, 32),
+            icon=self.get_icon("forum", self.SIZE_ICO),
+            iconSize=QSize(self.SIZE_ICO, self.SIZE_ICO),
         )
         btn.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
         btn.setMinimumWidth(160)
@@ -67,15 +71,15 @@ class HelloMain(QWidget):
         if menu := QMenu(self):
             action = menu.addAction(self.get_icon("forum"), "Forum")
             action.setIconVisibleInMenu(True)
-            menu.addAction(self.get_icon("application-certificate"), "Donate")
+            menu.addAction(self.get_icon("donate"), "Donate")
             btn.setMenu(menu)
-        layout.addWidget(btn, y, 1, alignment=Qt.Alignment.AlignCenter)
+        layout.addWidget(btn, y, 1, alignment=Qt.AlignmentFlag.AlignCenter)
 
         btn = QToolButton(
             parent=self,
             text="Project",
-            icon=self.get_icon("applications-development", 32),
-            iconSize=QSize(32, 32),
+            icon=self.get_icon("applications-development", self.SIZE_ICO),
+            iconSize=QSize(self.SIZE_ICO, self.SIZE_ICO),
             popupMode=QToolButton.ToolButtonPopupMode.InstantPopup,
         )
         btn.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
@@ -87,18 +91,61 @@ class HelloMain(QWidget):
             menu.addAction(self.get_icon("applications-development"), "Development")
             menu.addAction(self.get_icon("lists"), "Mailings lists")
             btn.setMenu(menu)
-        layout.addWidget(btn, y, 2, alignment=Qt.Alignment.AlignCenter)
+        layout.addWidget(btn, y, 2, alignment=Qt.AlignmentFlag.AlignCenter)
 
         y = 3
-        label = QLabel("...", parent=self, margin=5, alignment=Qt.Alignment.AlignCenter)
+        label = QLabel("...", parent=self, margin=5, alignment=Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label, y, 0)
 
-        label = QLabel("...", parent=self, margin=5, alignment=Qt.Alignment.AlignCenter)
+        label = QLabel("...", parent=self, margin=5, alignment=Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label, y, 1)
 
-        label = QLabel("...", parent=self, margin=5, alignment=Qt.Alignment.AlignCenter)
+        label = QLabel("...", parent=self, margin=5, alignment=Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label, y, 2)
 
     def get_icon(self, name: str, size: int = 22) -> QPixmap:
         """Get an icon from the assets directory."""
         return QIcon(str(Path(__file__).parent.parent / "assets" / f"{name}.svg")).pixmap(size, size)
+        ico = self.icon_from_svg(name, size)
+        return ico.pixmap(size, size)
+
+    def icon_from_svg(self, file_name: str, size: int = 48) -> QIcon:
+        """Create an icon from an SVG file."""
+        svg_path = Path(__file__).parent.parent / "assets" / f"{file_name}.svg"
+        if not svg_path.exists():
+            raise FileNotFoundError(f"SVG file {svg_path} does not exist.")
+
+        content = svg_path.read_text(encoding="utf-8")
+        if not content:
+            raise ValueError(f"SVG file {svg_path} is empty.")
+        # content = content.replace("style X", "style y") ?
+
+        width = 24
+        match = re.search(r'width="(\d+)"\s+height="(\d+)"', content)
+        if match:
+            width = float(match.groups()[0])
+        match = re.search(r'viewBox="0 0 (\d+(\.\d+)?) (\d+(\.\d+)?)"', content)
+        if match:
+            width = float(match.groups()[0])
+        r, w = width / 2, width / 8
+
+        # palette = QApplication.palette()
+        # color = palette.color(QPalette.ColorRole.Highlight).name()
+        color = "#35bf5c"
+        content = content.replace(
+            "</svg>",
+            f'<circle cx="{r}" cy="{r}" r="{r - w + 1}" stroke="{color}" stroke-width="{w}" fill="none"/></svg>',
+        )
+        print(file_name)
+        print()
+        print(content)
+
+        renderer = QSvgRenderer(content.encode("utf-8"))
+        pixmap = QPixmap(QSize(size, size))
+        pixmap.fill(Qt.GlobalColor.transparent)
+
+        painter = QPainter(pixmap)
+        renderer.render(painter)
+        painter.end()
+
+        return QIcon(pixmap)
